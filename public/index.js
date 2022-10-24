b06773da6895910228b89519795476a9c8e5dfff
Karl Rosengren <60710855+Goby56@users.noreply.github.com>
2022-10-24 15:23:49 +0200

    central username dictionary

--- public/index.js
+++ public/index.js
@@ -1,11 +1,15 @@
-var socket = io.connect('http://localhost:3000/');
+var username = prompt("Choose a username: ")
+// https://socket.io/docs/v4/middlewares/
+var socket = io.connect('http://localhost:3000/', );
+
+socket.emit("username handshake", username)
 
 var messages = document.getElementById('messages');
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 var typingDiv = document.getElementById("typingDiv")
 
-var username = prompt("Choose a username: ")
+var peopleTyping = []
 
 
 // Handle chat input
@@ -15,11 +19,16 @@ form.addEventListener('submit', function(e) {
         message = `${username}: ${input.value}`
         socket.emit('chat message', message);
         input.value = '';
+        socket.emit("stopped typing", username)
     }
 });
 
 input.addEventListener('input', () => {
-    socket.emit("someone typing", username)
+    if (input.value) {
+        socket.emit("typing", username)
+    } else {
+        socket.emit("stopped typing", username)
+    }
 })
 
 // Add message html
@@ -32,9 +41,18 @@ socket.on("user connection", (userID) => {
 socket.on("user disconnection", (userID) => {
     appendToChat(userID + " disconnected.")
 })
-socket.on("someone typing", (username) => {
-    typingDiv.innerHTML = username
-    console.log(username)
+socket.on("typing", (username) => {
+    if (!peopleTyping.includes(username)) {
+        peopleTyping.push(username)
+    }
+    updateTypingStatus(peopleTyping)
+})
+socket.on("stopped typing", (username) => {
+    if (peopleTyping.includes(username)) {
+        index = peopleTyping.find(username)
+        peopleTyping.pop(index)
+    }
+    updateTypingStatus(peopleTyping)
 })
 
 
@@ -44,4 +62,13 @@ function appendToChat(content) {
     item.textContent = content;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
+}
+
+// Update typing div
+function updateTypingStatus(peopleTyping) {
+    if (peopleTyping.length > 2) {
+        typingDiv.innerHTML = `${peopleTyping[0]}, ${peopleTyping[1]}, +${peopleTyping.length-2}...`
+    } else {
+        typingDiv.innerHTML = `${peopleTyping[0]}, ${peopleTyping[1]}...`
+    }
 }
--- public/server.js
+++ public/server.js
@@ -1,6 +1,9 @@
 // Setup
 const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
+
 const http = require('http');
 const server = http.createServer(app);
 
@@ -8,23 +11,37 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 }); 
 
+var users = {}
+var peopleTyping = []
+
+
+
 io.on('connection', (socket) => {
-    socket.broadcast.emit("user connection", socket.id)
+    socket.on("username handshake", (username) => {
+        users[socket.id] = username
+    })
+    socket.broadcast.emit("user connection", username)
 
 	socket.on('disconnect', () => {
-        socket.broadcast.emit("user disconnection", socket.id)
+        socket.broadcast.emit("user disconnection", username)
     });
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg)
     });
 
-    socket.on("someone typing", (username) => {
-        io.emit("someone typing", username)
+    socket.on("started typing", (username) => {
+        
+        io.emit("people typing", username)
+    })
+
+    socket.on("stopped typing", (username) => {
+        io.emit("stopped typing", username)
     })
 });
 
