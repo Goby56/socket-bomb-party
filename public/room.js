d97bdeb100c5e514295a5c7b7daa4877ab2d1a6e
Goby56 <60710855+Goby56@users.noreply.github.com>
2023-04-21 19:47:26 +0200

    agents

--- public/room.js
+++ public/room.js
@@ -1,4 +1,5 @@
 import { connectSocket } from "./auth.js"
+
 let socket = connectSocket()
 
 socket.emit("joinRoom", window.location.pathname.slice(-4), (responseCode) => {
@@ -7,19 +8,25 @@ socket.emit("joinRoom", window.location.pathname.slice(-4), (responseCode) => {
 
 let gameBoard = $("#game-board")
 
-for (let r = 1; r <= 5; r++) {
-    for (let c = 1; c <= 5; c++) {
+for (let r = 0; r < 5; r++) {
+    for (let c = 0; c < 5; c++) {
         gameBoard.append(`
-        <div class="agent-card" style="grid-area: ${r} / ${c} / ${r+1} / ${c+1};">
+        <div class="agent-card" style="grid-area: ${r+1} / ${c+1} / ${r+2} / ${c+2};">
             <div class="agent-card-inner">
                 <div class="agent-card-front">
-                    <p>${r}-${c}</p>
+                    <p class="name-label1">Hello</p>
+                    <p class="name-label2">my name is</p>
+                    <p class="agent-codename">${r} / ${c}</p>
                 </div>
                 <div class="agent-card-back">
-                    <img src="/images/agents/parick-bateman.jpg" alt="">
+                    <img src="/images/agents/${r*c + c}" alt="">
                 </div>
             </div>
         </div>
         `)
     }
 }
+
+$("#-reveal-all").on("click", e => {
+    $(".agent-card-inner").toggleClass("flip")
+})
